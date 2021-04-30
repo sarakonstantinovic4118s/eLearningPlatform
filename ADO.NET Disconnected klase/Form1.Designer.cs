6df511e9a3d23821e62019dfd2d65b083550dc22
Sara <skonstantinovic4118s@raf.rs>
2021-04-30 12:47:08 +0200

    Postavljanje projekta

---
+++ ADO.NET Disconnected klase/Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿
+namespace ADO.NET_Disconnected_klase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreateDataSet = new System.Windows.Forms.Button();
+            this.dgKupci = new System.Windows.Forms.DataGridView();
+            this.dgFakture = new System.Windows.Forms.DataGridView();
+            this.btnSaveXML = new System.Windows.Forms.Button();
+            this.dgFaktureStavke = new System.Windows.Forms.DataGridView();
+            this.txtNoviKupac = new System.Windows.Forms.TextBox();
+            this.lblNazivKupca = new System.Windows.Forms.Label();
+            this.lblAdresa = new System.Windows.Forms.Label();
+            this.txtAdresa = new System.Windows.Forms.TextBox();
+            this.btnDodajKupca = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtKupacID = new System.Windows.Forms.TextBox();
+            this.btnDodajFakturu = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtFakturaID = new System.Windows.Forms.TextBox();
+            this.txtNazivStavkeFakture = new System.Windows.Forms.TextBox();
+            this.txtCenaStavke = new System.Windows.Forms.TextBox();
+            this.btnDodajStavkuFakture = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dgKupci)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgFakture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgFaktureStavke)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCreateDataSet
+            // 
+            this.btnCreateDataSet.Location = new System.Drawing.Point(8, 11);
+            this.btnCreateDataSet.Name = "btnCreateDataSet";
+            this.btnCreateDataSet.Size = new System.Drawing.Size(128, 54);
+            this.btnCreateDataSet.TabIndex = 0;
+            this.btnCreateDataSet.Text = "Create DataSet";
+            this.btnCreateDataSet.UseVisualStyleBackColor = true;
+            this.btnCreateDataSet.Click += new System.EventHandler(this.btnCreateDataSet_Click);
+            // 
+            // dgKupci
+            // 
+            this.dgKupci.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgKupci.Location = new System.Drawing.Point(8, 82);
+            this.dgKupci.Name = "dgKupci";
+            this.dgKupci.RowHeadersWidth = 51;
+            this.dgKupci.RowTemplate.Height = 24;
+            this.dgKupci.Size = new System.Drawing.Size(306, 138);
+            this.dgKupci.TabIndex = 1;
+            // 
+            // dgFakture
+            // 
+            this.dgFakture.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgFakture.Location = new System.Drawing.Point(341, 81);
+            this.dgFakture.Name = "dgFakture";
+            this.dgFakture.RowHeadersWidth = 51;
+            this.dgFakture.RowTemplate.Height = 24;
+            this.dgFakture.Size = new System.Drawing.Size(315, 138);
+            this.dgFakture.TabIndex = 2;
+            // 
+            // btnSaveXML
+            // 
+            this.btnSaveXML.Location = new System.Drawing.Point(8, 438);
+            this.btnSaveXML.Name = "btnSaveXML";
+            this.btnSaveXML.Size = new System.Drawing.Size(128, 50);
+            this.btnSaveXML.TabIndex = 3;
+            this.btnSaveXML.Text = "Save XML";
+            this.btnSaveXML.UseVisualStyleBackColor = true;
+            this.btnSaveXML.Click += new System.EventHandler(this.btnSaveXML_Click);
+            // 
+            // dgFaktureStavke
+            // 
+            this.dgFaktureStavke.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgFaktureStavke.Location = new System.Drawing.Point(689, 83);
+            this.dgFaktureStavke.Name = "dgFaktureStavke";
+            this.dgFaktureStavke.RowHeadersWidth = 51;
+            this.dgFaktureStavke.RowTemplate.Height = 24;
+            this.dgFaktureStavke.Size = new System.Drawing.Size(290, 137);
+            this.dgFaktureStavke.TabIndex = 4;
+            // 
+            // txtNoviKupac
+            // 
+            this.txtNoviKupac.Location = new System.Drawing.Point(100, 239);
+            this.txtNoviKupac.Name = "txtNoviKupac";
+            this.txtNoviKupac.Size = new System.Drawing.Size(174, 22);
+            this.txtNoviKupac.TabIndex = 5;
+            // 
+            // lblNazivKupca
+            // 
+            this.lblNazivKupca.AutoSize = true;
+            this.lblNazivKupca.Location = new System.Drawing.Point(12, 244);
+            this.lblNazivKupca.Name = "lblNazivKupca";
+            this.lblNazivKupca.Size = new System.Drawing.Size(76, 17);
+            this.lblNazivKupca.TabIndex = 6;
+            this.lblNazivKupca.Text = "Ime kupca:";
+            // 
+            // lblAdresa
+            // 
+            this.lblAdresa.AutoSize = true;
+            this.lblAdresa.Location = new System.Drawing.Point(12, 280);
+            this.lblAdresa.Name = "lblAdresa";
+            this.lblAdresa.Size = new System.Drawing.Size(57, 17);
+            this.lblAdresa.TabIndex = 7;
+            this.lblAdresa.Text = "Adresa:";
+            // 
+            // txtAdresa
+            // 
+            this.txtAdresa.Location = new System.Drawing.Point(100, 275);
+            this.txtAdresa.Name = "txtAdresa";
+            this.txtAdresa.Size = new System.Drawing.Size(174, 22);
+            this.txtAdresa.TabIndex = 8;
+            // 
+            // btnDodajKupca
+            // 
+            this.btnDodajKupca.Location = new System.Drawing.Point(8, 350);
+            this.btnDodajKupca.Name = "btnDodajKupca";
+            this.btnDodajKupca.Size = new System.Drawing.Size(181, 40);
+            this.btnDodajKupca.TabIndex = 9;
+            this.btnDodajKupca.Text = "Dodaj kupca";
+            this.btnDodajKupca.UseVisualStyleBackColor = true;
+            this.btnDodajKupca.Click += new System.EventHandler(this.btnDodajKupca_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(338, 244);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "KupacID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(338, 280);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Datum:";
+            // 
+            // txtKupacID
+            // 
+            this.txtKupacID.Location = new System.Drawing.Point(423, 239);
+            this.txtKupacID.Name = "txtKupacID";
+            this.txtKupacID.Size = new System.Drawing.Size(94, 22);
+            this.txtKupacID.TabIndex = 12;
+            // 
+            // btnDodajFakturu
+            // 
+            this.btnDodajFakturu.Location = new System.Drawing.Point(423, 350);
+            this.btnDodajFakturu.Name = "btnDodajFakturu";
+            this.btnDodajFakturu.Size = new System.Drawing.Size(167, 40);
+            this.btnDodajFakturu.TabIndex = 14;
+            this.btnDodajFakturu.Text = "Dodaj fakturu";
+            this.btnDodajFakturu.UseVisualStyleBackColor = true;
+            this.btnDodajFakturu.Click += new System.EventHandler(this.btnDodajFakturu_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(686, 244);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 17);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "FakturaID:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(686, 280);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(140, 17);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Naziv stavke fakture:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(686, 318);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 17);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Cena stavke:";
+            // 
+            // txtFakturaID
+            // 
+            this.txtFakturaID.Location = new System.Drawing.Point(843, 239);
+            this.txtFakturaID.Name = "txtFakturaID";
+            this.txtFakturaID.Size = new System.Drawing.Size(84, 22);
+            this.txtFakturaID.TabIndex = 18;
+            // 
+            // txtNazivStavkeFakture
+            // 
+            this.txtNazivStavkeFakture.Location = new System.Drawing.Point(843, 275);
+            this.txtNazivStavkeFakture.Name = "txtNazivStavkeFakture";
+            this.txtNazivStavkeFakture.Size = new System.Drawing.Size(136, 22);
+            this.txtNazivStavkeFakture.TabIndex = 19;
+            // 
+            // txtCenaStavke
+            // 
+            this.txtCenaStavke.Location = new System.Drawing.Point(843, 313);
+            this.txtCenaStavke.Name = "txtCenaStavke";
+            this.txtCenaStavke.Size = new System.Drawing.Size(122, 22);
+            this.txtCenaStavke.TabIndex = 20;
+            // 
+            // btnDodajStavkuFakture
+            // 
+            this.btnDodajStavkuFakture.Location = new System.Drawing.Point(785, 350);
+            this.btnDodajStavkuFakture.Name = "btnDodajStavkuFakture";
+            this.btnDodajStavkuFakture.Size = new System.Drawing.Size(194, 40);
+            this.btnDodajStavkuFakture.TabIndex = 21;
+            this.btnDodajStavkuFakture.Text = "Dodaj stavku fakture";
+            this.btnDodajStavkuFakture.UseVisualStyleBackColor = true;
+            this.btnDodajStavkuFakture.Click += new System.EventHandler(this.btnDodajStavkuFakture_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "hh:mm:ss dd-MM-yyyy";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(423, 280);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(233, 22);
+            this.dateTimePicker1.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1003, 500);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.btnDodajStavkuFakture);
+            this.Controls.Add(this.txtCenaStavke);
+            this.Controls.Add(this.txtNazivStavkeFakture);
+            this.Controls.Add(this.txtFakturaID);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnDodajFakturu);
+            this.Controls.Add(this.txtKupacID);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDodajKupca);
+            this.Controls.Add(this.txtAdresa);
+            this.Controls.Add(this.lblAdresa);
+            this.Controls.Add(this.lblNazivKupca);
+            this.Controls.Add(this.txtNoviKupac);
+            this.Controls.Add(this.dgFaktureStavke);
+            this.Controls.Add(this.btnSaveXML);
+            this.Controls.Add(this.dgFakture);
+            this.Controls.Add(this.dgKupci);
+            this.Controls.Add(this.btnCreateDataSet);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgKupci)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgFakture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgFaktureStavke)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreateDataSet;
+        private System.Windows.Forms.DataGridView dgKupci;
+        private System.Windows.Forms.DataGridView dgFakture;
+        private System.Windows.Forms.Button btnSaveXML;
+        private System.Windows.Forms.DataGridView dgFaktureStavke;
+        private System.Windows.Forms.TextBox txtNoviKupac;
+        private System.Windows.Forms.Label lblNazivKupca;
+        private System.Windows.Forms.Label lblAdresa;
+        private System.Windows.Forms.TextBox txtAdresa;
+        private System.Windows.Forms.Button btnDodajKupca;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtKupacID;
+        private System.Windows.Forms.Button btnDodajFakturu;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtFakturaID;
+        private System.Windows.Forms.TextBox txtNazivStavkeFakture;
+        private System.Windows.Forms.TextBox txtCenaStavke;
+        private System.Windows.Forms.Button btnDodajStavkuFakture;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ ADO.NET Disconnected klase/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADO.NET_Disconnected_klase
+{
+    public partial class Form1 : Form
+    {
+        DataTable kupci, fakture, faktureStavke;
+        DataSet fakturisanje;
+
+        private void btnSaveXML_Click(object sender, EventArgs e)
+        {
+            // Omogućiti serijalizaciju sva tri entiteta u jedan XML fajl koji treba da sadrži XML šemu i podatke
+            fakturisanje.WriteXml(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\fakture.xml", XmlWriteMode.WriteSchema);
+            MessageBox.Show("Successfully saved!");
+        }
+
+        private void btnDodajKupca_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataRow noviKupac = kupci.NewRow();
+                if (txtNoviKupac.Text == "" || txtAdresa.Text == "")
+                {
+                    MessageBox.Show("Polje je prazno, molimo unesite vrednost!");
+                }
+                else
+                {
+                    noviKupac["NazivKupca"] = txtNoviKupac.Text;
+                    noviKupac["Adresa"] = txtAdresa.Text;
+                    kupci.Rows.Add(noviKupac);
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Doslo je do greske prilikom unosa kupca, molimo pokusajte ponovo!");
+            }
+
+            txtNoviKupac.Text = "";
+            txtAdresa.Text = "";
+        }
+
+        private void btnDodajFakturu_Click(object sender, EventArgs e)
+        {
+            DataRow novaFaktura = fakture.NewRow();
+
+            try { 
+                if(txtKupacID.Text == "")
+                {
+                    MessageBox.Show("Polje je prazno, molimo unesite vrednost!");
+                }
+                else
+                {
+                    novaFaktura["KupacID"] = int.Parse(txtKupacID.Text);
+                    novaFaktura["Datum"] = dateTimePicker1.Text;
+                    fakture.Rows.Add(novaFaktura);
+                    
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Doslo je do greske prilikom unosa fakture, molimo pokusajte ponovo!");
+            }
+
+            txtKupacID.Text = "";
+            txtNoviKupac.Text = "";
+        }
+
+        private void btnDodajStavkuFakture_Click(object sender, EventArgs e)
+        {
+            DataRow novaStavka = faktureStavke.NewRow();
+
+            try
+            {
+                if(txtFakturaID.Text == "" || txtCenaStavke.Text == "" || txtNazivStavkeFakture.Text == "")
+                {
+                    MessageBox.Show("Polje je prazno, molimo unesite vrednost!");
+                }
+                else
+                {
+                    novaStavka["FakturaID"] = int.Parse(txtFakturaID.Text);
+                    novaStavka["NazivStavke"] = txtNazivStavkeFakture.Text;
+                    novaStavka["CenaStavke"] = decimal.Parse(txtCenaStavke.Text);
+                    faktureStavke.Rows.Add(novaStavka);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Doslo je do greske prilikom unosa fakture, molimo pokusajte ponovo!");
+            }
+
+            txtFakturaID.Text = "";
+            txtNazivStavkeFakture.Text = "";
+            txtCenaStavke.Text = "";
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Entitet Kupci
+            kupci = new DataTable("Kupci");
+
+            // autonumber - KupacID, int, autoincrement (počev od 1, korak 1), nisu dozvoljene NULL vrednosti, primarni ključ
+            DataColumn idKup = new DataColumn("KupacID")
+            {
+                DataType = typeof(int),
+                AutoIncrement = true,
+                AutoIncrementSeed = 1, // po default-u
+                AutoIncrementStep = 1, // po default-u
+                AllowDBNull = false
+            };
+            kupci.Columns.Add(idKup);
+            kupci.PrimaryKey = new DataColumn[] { idKup };
+
+            // NazivKupca, string maksimalne dužine 50 karaktera, nisu dozvoljene NULL vrednosti
+            DataColumn naziv = new DataColumn("NazivKupca")
+            {
+                DataType = typeof(string),
+                MaxLength = 50,
+                AllowDBNull = false
+            };
+            kupci.Columns.Add(naziv);
+
+            // Adresa, string maksimalne dužine 200 karaktera, dozvoljene su NULL vrednosti
+            DataColumn adr = new DataColumn("Adresa")
+            {
+                DataType = typeof(string),
+                MaxLength = 200,
+                AllowDBNull = true
+            };
+            kupci.Columns.Add(adr);
+
+            // Entitet Fakture
+            fakture = new DataTable("Fakture");
+
+            // FakturaID, int, autoincrement (počev od 1, korak 1), nisu dozvoljene NULL vrednosti, primarni ključ
+            DataColumn idFakt = new DataColumn("FakturaID")
+            {
+                DataType = typeof(int),
+                AutoIncrement = true,
+                AutoIncrementSeed = 1, // po default-u
+                AutoIncrementStep = 1, // po default-u
+                AllowDBNull = false
+            };
+            fakture.Columns.Add(idFakt);
+            fakture.PrimaryKey = new DataColumn[] { idFakt };
+
+            // KupacID, int, nisu dozvoljene NULL vrednosti, spoljni ključ - referiše se na entitet Kupci, kolonu KupacID
+            DataColumn idKupF = new DataColumn("KupacID")
+            {
+                DataType = typeof(int),
+                AllowDBNull = false
+            };
+            fakture.Columns.Add(idKupF);
+
+            // Datum, datetime, nisu dozvoljene NULL vrednosti, default vrednost je trenutni datum i vreme
+            DataColumn datum = new DataColumn("Datum")
+            {
+                DataType = typeof(DateTime),
+                DefaultValue = DateTime.Now,
+                AllowDBNull = false
+            };
+            fakture.Columns.Add(datum);
+
+            // Entitet FaktureStavke
+            faktureStavke = new DataTable("FaktureStavke");
+
+            // FakturaID, int, nisu dozvoljene NULL vrednosti, spoljni ključ - referiše se na entitet Fakture, kolonu FakturaID
+            DataColumn idFaktStav = new DataColumn("FakturaID")
+            {
+                DataType = typeof(int),
+                AllowDBNull = false
+            };
+            faktureStavke.Columns.Add(idFaktStav);
+
+            // NazivStavke, string maksimalne dužine 40 karaktera, nisu dozvoljene NULL vrednosti
+            DataColumn nazivStavke = new DataColumn("NazivStavke") {
+                DataType = typeof(string),
+                MaxLength = 40,
+                AllowDBNull = false
+            };
+            faktureStavke.Columns.Add(nazivStavke);
+
+            // CenaStavke, decimal, nisu dozvoljene NULL vrednosti
+            DataColumn cenaStavke = new DataColumn("CenaStavke"){
+                DataType = typeof(decimal),
+                AllowDBNull = false
+            };
+            faktureStavke.Columns.Add(cenaStavke);
+            
+            // Primarni ključ ovog entiteta je kompozitni i sastoji se od atributa FakturaID i NazivStavke
+            faktureStavke.PrimaryKey = new DataColumn[] { idFaktStav, nazivStavke };
+
+            // ubacivanje u data set
+            fakturisanje = new DataSet();
+            fakturisanje.Tables.Add(kupci);
+            fakturisanje.Tables.Add(fakture);
+            fakturisanje.Tables.Add(faktureStavke);
+
+            // pravljenje relacije1
+            DataRelation dataRelation1 = new DataRelation("RelacijaKupciFakture", kupci.Columns["KupacID"], fakture.Columns["KupacID"], true);
+            fakturisanje.Relations.Add(dataRelation1);
+
+            ForeignKeyConstraint fk = (ForeignKeyConstraint)fakture.Constraints["RelacijaKupciFakture"];
+
+            // pravljenje relacije2
+            DataRelation dataRelation2 = new DataRelation("RelacijaFaktureStavke", fakture.Columns["FakturaID"], faktureStavke.Columns["FakturaID"], true);
+            fakturisanje.Relations.Add(dataRelation2);
+
+            ForeignKeyConstraint fk2 = (ForeignKeyConstraint)faktureStavke.Constraints["RelacijaFaktureStavke"];
+
+            // Za obe relacije postaviti UPDATE i DELETE RULE na none.
+            fk.DeleteRule = Rule.None;
+            fk.UpdateRule = Rule.None;
+            fk2.DeleteRule = Rule.None;
+            fk2.UpdateRule = Rule.None;
+
+            // dodavanje par slogova
+            kupci.Rows.Add(1, "Kupac 1", "Adresa 1");
+            kupci.Rows.Add(2, "Kupac 2", "Adresa 2");
+            kupci.Rows.Add(3, "Kupac 3", "Adresa 3");
+
+            fakture.Rows.Add(1, 1);
+            fakture.Rows.Add(2, 2, "15-4-2021 16:00:00");
+            fakture.Rows.Add(3, 3, "10-4-2021 09:00:00");
+
+
+            faktureStavke.Rows.Add(1, "Stavka 1", 1000);
+            faktureStavke.Rows.Add(2, "Stavka 2", 2000);
+        }
+
+        private void btnCreateDataSet_Click(object sender, EventArgs e)
+        {
+            try{
+                // U Windows Forms tipu aplikacije pomoću DataGridView kontrola, prikazati 
+                // podatke iz sva tri entiteta. Sve uraditi na jednoj formi.
+                dgKupci.DataSource = kupci;
+                dgFakture.DataSource = fakture;
+                dgFaktureStavke.DataSource = faktureStavke;
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Doslo je do greske prilikom ucitavanja tabele!");
+            }
+        }
+    }
+}
