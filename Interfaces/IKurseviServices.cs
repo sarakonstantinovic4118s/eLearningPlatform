12bcc896d7489a45af297e24b9c027ae5af576f7
Ivan Todorovic <todorovicivan99@yahoo.com>
2021-05-03 21:37:03 +0200

    kursevi-pocetak2

--- Interfaces/IKurseviServices.cs
+++ Interfaces/IKurseviServices.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using eLearning.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace eLearning.Interfaces
 {
-    interface IKurseviServices
+    public interface IKurseviServices
     {
+        List<Kursevi> Read();
     }
 }
