0ac486a99a633913c0ba921f9ed849be6821a004
OgnjenPopovicS17-18 <opopovic1718s@raf.rs>
2021-05-03 22:34:36 +0200

    Dodata stranica za kurseve

--- Services/KurseviServices.cs
+++ Services/KurseviServices.cs
@@ -1,11 +1,30 @@
-﻿using System;
+﻿using eLearning.Interfaces;
+using eLearning.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace eLearning.Services
 {
-    public class KurseviServices
+    public class KurseviServices : IKurseviServices
     {
+        private readonly IMongoCollection<Kursevi> kursevi;
+
+        public KurseviServices(IDatabaseSettings podesavanja)
+        {
+            var client = new MongoClient(podesavanja.ConnectionString);
+            var database = client.GetDatabase(podesavanja.DatabaseName);
+            this.kursevi = database.GetCollection<Kursevi>("kursevi");
+        }
+
+        public List<Kursevi> Read()
+        {
+            // selektovanje svih korisnika
+            var k = kursevi.Find(k => true);
+            return k.ToList();
+        }
     }
 }
