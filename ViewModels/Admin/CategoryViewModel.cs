ed7e631a45ad961c73c297fc00d7fe0b4f47b1a6
btstefan <test@test1>
2021-05-18 17:20:02 +0200

    Validacija forme za admin panel

---
+++ ViewModels/Admin/CategoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eLearning.ViewModels.Admin
+{
+    public class CategoryViewModel
+    {
+        [Required]
+        public string naziv { get; set; }
+    }
+}
